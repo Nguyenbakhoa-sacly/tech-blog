6ed751f54e3578bc1ea834011cebdc5406ad191b
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-01-31 10:56:40 +0700

    connect to the database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
 const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+mongoose.connect(process.env.MONGOBD_URL)
+  .then(() => {
+    console.log('Mongodb connection successful');
+  });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
