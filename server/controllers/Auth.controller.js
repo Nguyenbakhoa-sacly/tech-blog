712e0a7140244935bd82b42a54d42c1f6482e8f6
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-02-03 01:40:34 +0700

    feature(server): create API signIn

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -2,6 +2,11 @@
 const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
 const { errorHandler } = require('../utils/error')
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken');
+
+dotenv.config();
+
 const authController = {
 
   signUp: async (req, res, next) => {
@@ -22,6 +27,33 @@ const authController = {
       return res.status(200).json({
         message: 'SignUp successfully!'
       })
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  signIn: async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === '' || password === '') {
+      next(errorHandler(400, ' All fields are required'));
+    }
+    try {
+      const validUser = await User.findOne({ email: email });
+      if (!validUser) {
+        return next(errorHandler(400, 'User not found!'));
+      }
+      const validPassword = bcrypt.compareSync(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHandler(400, 'Invalid password'));
+      }
+      const token = jwt.sign(
+        { userId: validUser._id, username: validUser.username },
+        process.env.SECRET_KEY
+      )
+      // không trả ra password
+      const { password: pass, ...rest } = validUser._doc;
+      return res.status(200).cookie('access_token', token,
+        { httpOnly: true }).json(rest);
     } catch (e) {
       next(e);
     }
--- server/routes/Auth.route.js
+++ server/routes/Auth.route.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 const authController = require('../controllers/Auth.controller')
 
 router.post('/signup', authController.signUp)
+router.post('/signin', authController.signIn)
 
 module.exports = router
