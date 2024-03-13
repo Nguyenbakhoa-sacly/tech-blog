bf88a9c9354bb6b050ede10f0ea67b162ee8dec6
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2024-03-13 23:52:47 +0700

    UI: complete the About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      about
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            Technology' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              <strong>Xin chào! </strong> Tôi là Nguyễn Bá Khoa, một người đam mê mãnh liệt với công nghệ. Blog này là nơi tôi chia sẻ những suy nghĩ, ý tưởng và kiến thức về lĩnh vực mà tôi yêu thích này với mọi người.
+              Tại đây, bạn sẽ tìm thấy các bài viết và hướng dẫn được cập nhật thường xuyên về nhiều chủ đề liên quan đến công nghệ, <strong>bao gồm phát triển web</strong>, <strong>lập trình phần mềm</strong>,<strong>các ngôn ngữ lập trình</strong>, và nhiều hơn nữa!
+              Mục tiêu của blog là chia sẻ kiến thức và kinh nghiệm tôi tích lũy được trong suốt quá trình học tập và làm việc trong ngành công nghệ, truyền cảm hứng cho mọi người cùng khám phá và chinh phục thế giới công nghệ đầy tiềm năng.
+              Hãy tham gia cùng tôi trong hành trình khám phá thế giới công nghệ đầy thú vị! Hãy đọc các bài viết, để lại bình luận và chia sẻ ý tưởng của bạn.
+            </p>
+            <p> Cảm ơn bạn đã ghé thăm blog của tôi!</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
